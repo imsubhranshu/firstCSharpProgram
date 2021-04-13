459b2383850898217ad78fb57b1baf6cbf9588d0
imsubhranshu <subhranshu.behera99.sb@gmail.com>
2021-04-13 12:07:30 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+            Console.WriteLine("Taking Input");
+            string data = Console.ReadLine();
+            Console.Write("The value entered is:\n" + data);
+        }
+    }
+}
